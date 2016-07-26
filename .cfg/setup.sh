af86892459c010b3a25384247fd990b80990ee18
Alistair Reid <alistair.reid@nicta.com.au>
2016-07-26 18:51:37 +1000

    Further moves

--- .cfg/setup.sh
+++ .cfg/setup.sh
@@ -1,21 +1,6 @@
-mkdir .cfg
-
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-
-config clone --bare https://github.com/AlistaiReid/dotfiles.git
-
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+mkdir $HOME/.cfg
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/AlistaiReid/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 config config status.showUntrackedFiles no
-
-
-
